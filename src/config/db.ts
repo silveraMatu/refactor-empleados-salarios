282272f3d7f86ecc7878ccf1ca80b2b786531c67
silveraMatu <matusilvera808@gmail.com>
2026-09-10 00:25:20 -0300

    fix: se cambio la versión de mongodb utilizada por una estable (de 8 a 7)y compatible con las últimas versiones del kernel de linux. Ahora si se conecta a la db

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async (): Promise<void> => {
   try {
     const MONGO_URI =
-      process.env.MONGO_URI ?? "mongodb://localhost:27017/employees_db";
+      process.env.MONGO_URI ?? "mongodb://127.0.0.1:27017/employees_db";
     await mongoose.connect(MONGO_URI);
     console.log("Se conectó exitosamente con MongoDB");
   } catch (error) {
